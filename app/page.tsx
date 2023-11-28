d796e7161167c3fc5768d5c2f89f0168104cdc4f
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-11-28 03:16:41 +0100

    fix:deployment issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Homepage from "@/pages";
+import BestDeals from "@/app/Components/BestDeals";
+import Hero from "@/app/Components/Hero";
+import Layout from "@/app/Components/Layout";
 
-export default function Home() {
+import React from "react";
+
+const bestDeals = [
+  { id: 1, name: "Deal 1", price: 15.99 },
+  { id: 2, name: "Deal 2", price: 25.99 },
+  { id: 3, name: "Deal 3", price: 35.99 },
+];
+
+const Homepage: React.FC = () => {
   return (
-    <main className="">
-      <Homepage />
-    </main>
+    <Layout>
+      <Hero />
+      <BestDeals products={bestDeals} />
+    </Layout>
   );
-}
+};
+
+export default Homepage;

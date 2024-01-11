5298f030586fb39013a23f98a99cf0db94e6f087
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-01-11 05:42:35 +0100

    update

--- pages/products.tsx
+++ pages/products.tsx
@@ -26,7 +26,11 @@ const Products: React.FC<ProductsProps> = () => {
             <div key={product.id} className="bg-white p-4 rounded-md shadow-md">
               <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
               <p className="text-gray-600 mb-2">N{product.price.toFixed(2)}</p>
-              <DynamicAddToCartButton id={product.id} />
+              <DynamicAddToCartButton
+                id={product.id}
+                name={product.name}
+                price={product.price}
+              />
             </div>
           ))}
         </div>

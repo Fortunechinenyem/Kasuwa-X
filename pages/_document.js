d2af92cc76004fa11352f86da5765c150d8a7f8d
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-01-11 05:31:07 +0100

    fix:deployment issues

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Gordita:wght@400;700&display=swap"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+          />
         </Head>
         <body>
           <Main />

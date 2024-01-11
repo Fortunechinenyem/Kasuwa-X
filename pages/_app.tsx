dd9f18939b953751728054b6b51602f17990fb72
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-01-11 05:25:14 +0100

    fix:deployment issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,32 +14,6 @@ const KasuwaX = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Gordita:wght@400;700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
-        />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <title>{metadata?.title}</title>

9b1202c580c7aa821731a3c9876cd1f5afd3b0e2
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-01-13 05:44:01 +0100

    fix:deploy issues

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { SessionProvider, useSession } from "next-auth/react"; // Import SessionProvider
+
 import Layout from "@/app/Layouts/DefaultLayout";
 
 const Register: React.FC = () => {
   const router = useRouter();
-  const { data: session } = useSession();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -14,13 +13,6 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    // Check if the user is already authenticated
-    if (session) {
-      // Redirect to a dashboard or home page for authenticated users
-      router.push("/");
-      return;
-    }
 
     // Call NextAuth.js API for registration
     const response = await fetch("/api/auth/signup", {
@@ -47,67 +39,65 @@ const Register: React.FC = () => {
 
   return (
     <Layout>
-      <SessionProvider session={session}>
-        {" "}
-        {/* Wrap your component with SessionProvider */}
-        <div className="container mx-auto my-8">
-          <h2 className="text-3xl font-semibold mb-4 text-center">Register</h2>
-          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-            <label className="block mb-2">
-              Email:
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
-                required
-              />
-            </label>
+      {" "}
+      {/* Wrap your component with SessionProvider */}
+      <div className="container mx-auto my-8">
+        <h2 className="text-3xl font-semibold mb-4 text-center">Register</h2>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <label className="block mb-2">
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
+              required
+            />
+          </label>
 
-            <label className="block mb-2">
-              Password:
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
-                required
-              />
-            </label>
+          <label className="block mb-2">
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
+              required
+            />
+          </label>
 
-            <label className="block mb-2">
-              Confirm Password:
-              <input
-                type="password"
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
-                required
-              />
-            </label>
+          <label className="block mb-2">
+            Confirm Password:
+            <input
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
+              required
+            />
+          </label>
 
-            <label className="block mb-4">
-              Role:
-              <select
-                name="role"
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
-              >
-                <option value="customer">Customer</option>
-                <option value="vendor">Vendor</option>
-              </select>
-            </label>
+          <label className="block mb-4">
+            Role:
+            <select
+              name="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500"
+            >
+              <option value="customer">Customer</option>
+              <option value="vendor">Vendor</option>
+            </select>
+          </label>
 
-            <button type="submit" className="w-full button mt-7 mb-5 ">
-              Register
-            </button>
-          </form>
-        </div>
-      </SessionProvider>
+          <button type="submit" className="w-full button mt-7 mb-5 ">
+            Register
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
